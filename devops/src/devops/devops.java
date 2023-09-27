ecc5902e4fe97ec022db82bf797fe9fd5e4e78cd
DELL <DELL@DESKTOP-C26RVUN>
2023-09-27 14:54:54 +0530

    commit

---
+++ devops/src/devops/devops.java
@@ -0,0 +1,9 @@
+package devops;
+
+public class devops {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is a devops repository ");
+	}
+}
